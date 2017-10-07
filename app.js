892e989a73b73ed5210a5d899ef73c70755332f2
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2017-10-07 07:53:28 +0200

    Task #1 Removing unused jade views and routes

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//var sample = require('./routes/sample');
 
 var app = express();
 
@@ -22,8 +21,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//// Nuevas Rutas van aqui:
+//app.use('/sample', sample);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/sample.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* Sample REST service  (placeholder) */
+router.get('/sample', function(req, res, next) {
+  // TODO
+});
+
+module.exports = router;
