fd15aef9bf54e101ad244565b75224cf8c3d1d33
oscar s <oscar-su@live.fr>
2017-10-10 00:04:57 +0200

    customer-module

---
+++ public/app/customer-module/customer-module.component.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('customerModule')
+    .component('customerModule', {
+        templateUrl: '/app/customer-module/customer-module.html',
+        controller: function ($scope, $http) {
+            console.log("Incializando customer-module");
+
+
+        }
+    });
+
+    
