5af5d2ea3bbdd248f56d631d400006113b12d2bd
Óscar Suárez <oscar-su@live.fr>
2017-10-10 13:58:05 +0200

    redenrizar lista api

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -1,43 +1,17 @@
 'use strict';
 
-var angularRoutingApp = angular.module('petStore', ['ngRoute']);
-
-angularRoutingApp.config(function ($routeProvider, $locationProvider) {
-
-    $routeProvider
-
-        .when('/customers', {
-            templateUrl: 'customer/module/customer-module.html',
-            controller: 'lastNewsController'
-        })
-        .when('/news:id', {
-            templateUrl: 'views/oneNews.html',
-            controller: 'oneNewsController'
-        });
-
-    $locationProvider.html5Mode(true);
-});
-
-
-angularRoutingApp.controller('MainController', function ($scope) {
-
-}).controller('lastNewsController', function ($scope, $http) {
-    var lastNews = this;
-
-    $http.get('/api/news/news.json').then(function (response) {
-        lastNews.news = response.data;
-
-        console.log("puto json" + lastNews.news);
+angular.module('petStore')
+    .config(function ($locationProvider,
+                      $routeProvider) {
+        $locationProvider.html5Mode({enabled: true});
+        $routeProvider
+            .when("/", {
+                template: "<h2>Bienvenido a la App Clínica veterinaria</h2>"
+            })
+            .when("/customers", {
+                template: "<customer-module></customer-module>"
+            })
+            .otherwise({
+                template: "Other"
+            });
     });
-
-
-});
-
-angularRoutingApp.controller('oneNewsController', function ($scope, $http, $routeParams) {
-    var id = $routeParams.id;
-    var newsdetail = this;
-    $http.get('/api/news/' + id + '/news.json').then(function (response) {
-        newsdetail.oneNews = response.data
-    });
-
-});
